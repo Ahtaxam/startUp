26a4eabb033ecdd365b9ad78d2b3fbb0b0646028
Ahtaxam <Ahtasham.Ali@stackupsolutions.com>
2024-05-09 13:25:59 +0500

    add search for Investor

--- src/screens/Investor/Home/index.jsx
+++ src/screens/Investor/Home/index.jsx
@@ -1,35 +1,61 @@
-import React from 'react'
-import { useGetAllStudentsQuery } from '../../../redux/slices/Students'
-import { Loader } from '../../../components/Loader';
-import StudentCard from '../../../components/StudentCard';
-import { useNavigate } from 'react-router-dom';
-import { PATH } from '../../../utils/Path';
-import { Header } from '../../../components/Header';
+import React, { useState } from "react";
+import { useGetAllStudentsQuery } from "../../../redux/slices/Students";
+import { Loader } from "../../../components/Loader";
+import StudentCard from "../../../components/StudentCard";
+import { useNavigate } from "react-router-dom";
+import { PATH } from "../../../utils/Path";
+import { Header } from "../../../components/Header";
 
 function InvestorHome() {
-    const {data, isLoading} = useGetAllStudentsQuery();
-    const navigate = useNavigate();
-    const handleStudentDetails = (obj) => {
-       navigate(`/studentDetail/${obj._id}`)
-    }
-    
+  const [searchText, setSearchText] = useState("");
+  const { data, isLoading } = useGetAllStudentsQuery();
+  const navigate = useNavigate();
+  const handleStudentDetails = (obj) => {
+    navigate(`/studentDetail/${obj._id}`);
+  };
+  const handleSearchProject = (e) => {
+    setSearchText(e.target.value);
+  };
+  const filteredUsers = data?.data.filter((user) =>
+    user.Projects.some((project) =>
+      project.keywords.some((keyword) =>
+        keyword.toLowerCase().includes(searchText.toLowerCase())
+      )
+    )
+  );
+
   return (
     <div>
-      <Header/>
-        <p className='text-center m-4 text-3xl font-mono'>Students</p>
-        {isLoading ? (
+      <Header />
+      <p className="text-center m-4 text-3xl font-mono">Students</p>
+      <div className="flex m-4">
+        <input
+          type="search"
+          className="rounded-lg min-w-[400px]"
+          placeholder="Search based on technology"
+          value={searchText}
+          onChange={handleSearchProject}
+        />
+      </div>
+      {isLoading ? (
         <div className="flex justify-center items-center">
           <Loader />
         </div>
-      ) : (
+      ) : filteredUsers && filteredUsers.length > 0 ? (
         <div className=" p-2 grid grid-cols-1 sm:grid-cols-2  gap-4 mt-2 ]">
-          {data?.data.map((obj) => (
-            <StudentCard data={obj} onClick={() => handleStudentDetails(obj)} key={obj._id}></StudentCard>
+          {filteredUsers.map((obj) => (
+            <StudentCard
+              data={obj}
+              onClick={() => handleStudentDetails(obj)}
+              key={obj._id}
+            ></StudentCard>
           ))}
         </div>
+      ) : (
+        <p className="text-center font-inter text-xl">No matching!</p>
       )}
     </div>
-  )
+  );
 }
 
-export default InvestorHome
+export default InvestorHome;
--- src/screens/student/jobs/index.jsx
+++ src/screens/student/jobs/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useGetAllJobsQuery } from "../../../redux/slices/CreateJob";
 import { Loader } from "../../../components/Loader";
 import Card from "../../../components/Card";
@@ -6,24 +6,40 @@ import { useNavigate } from "react-router-dom";
 import { Header } from "../../../components/Header";
 
 function Jobs() {
+  const [searchText, setSearchText] = useState("");
   const { data, isLoading } = useGetAllJobsQuery();
   const navigate = useNavigate();
-  
 
   const handleCardClick = (obj) => {
     navigate(`/jobDetail/${obj.jobId}`);
   };
+  const handleSearchJob = (e) => {
+    setSearchText(e.target.value);
+  };
+
+  const filteredJobs = data?.data.filter((job) =>
+    job.title.toLowerCase().includes(searchText.toLowerCase())
+  );
   return (
     <div>
-        <Header/>
+      <Header />
       <p className="font-inter text-center text-2xl font-bold m-4">Jobs</p>
+      <div className="flex m-4">
+        <input
+          type="search"
+          className="rounded-lg min-w-[400px]"
+          placeholder="Search Job"
+          value={searchText}
+          onChange={handleSearchJob}
+        />
+      </div>
       {isLoading ? (
         <div className="flex justify-center items-center">
           <Loader />
         </div>
-      ) : (
+      ) : filteredJobs && filteredJobs.length > 0 ? (
         <div className=" p-2 grid grid-cols-1 sm:grid-cols-2  gap-4 mt-2 ]">
-          {data?.data.map((obj) => (
+          {filteredJobs?.map((obj) => (
             <Card
               data={obj}
               key={obj._id}
@@ -31,6 +47,10 @@ function Jobs() {
             />
           ))}
         </div>
+      ) : (
+        <p className="font-inter text-center text-lg">
+          Sorry what you are looking for, not found
+        </p>
       )}
     </div>
   );
